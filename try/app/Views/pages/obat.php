e1738982bbacc9afef6235300bf3a6fcca97e33a
Alfan Fabio <s21910113@student.unklab.ac.id>
2022-09-28 06:56:07 +0800

    menambahkan file baru

--- try/app/Views/pages/obat.php
+++ try/app/Views/pages/obat.php
@@ -3,81 +3,109 @@
 <?= $this->section('content'); ?>
 
 <div class="col">
-    <form class="example center" action="action_page.php">
-        <input type="text" placeholder="Search.." name="search">
-        <button type="submit"><i class="fa fa-search"></i></button>
-    </form>
+    <br>
+
+    </br>
 
 </div>
 
 <div class="container-fluid">
 
-    <div style="overflow-y: scroll;">
-        <div class="row">
-            <div class="col">
-                <h1 class="mt-2">Data Obat</h1>
-                <table class="table table-bordered" id="myTable">
-                    <thead>
-                        <tr class="header">
-                            <th scope="col">No.</th>
-                            <th scope="col">Nama Obat</th>
-                            <th scope="col">Jenis Obat</th>
-                            <th scope="col">Jumlah Obat</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $i = 1 ?>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Fixed Header Table</h3>
 
-                        <tr>
-                            <th scope="row"><?= $i++; ?></th>
-                            <td>AC Swabs</td>
-                            <td>Obat APBD</td>
-                            <td>16 Box</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><?= $i++; ?></th>
-                            <td>Alat Suntik Sekali Pakai 10 ml</td>
-                            <td>Obat APBD</td>
-                            <td> 18 Pack</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><?= $i++; ?></th>
-                            <td>Alat Suntik Sekali Pakai 3 ml</td>
-                            <td>Obat APBD</td>
-                            <td> 9 Kotak)</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><?= $i++; ?></th>
-                            <td>Alat Suntik Sekali Pakai 5 ml</td>
-                            <td>Obat APBD</td>
-                            <td> 93 Kotak)</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><?= $i++; ?></th>
-                            <td>Asm Sling Comfort</td>
-                            <td>Obat APBD</td>
-                            <td> 93 Pack)</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><?= $i++; ?></th>
-                            <td>Aximed Female Cateter No 16</td>
-                            <td>Obat APBD</td>
-                            <td> 1980 Pack)</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><?= $i++; ?></th>
-                            <td>Aximed Female Cateter No 18</td>
-                            <td>Obat APBD</td>
-                            <td> 370 Pack)</td>
-                        </tr>
+                    <div class="card-tools">
+                        <div class="input-group input-group-sm" style="width: 150px;">
+                            <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
 
-                    </tbody>
-                </table>
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body table-responsive p-0" style="height: 300px;">
+                    <table class="table table-head-fixed text-nowrap">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>User</th>
+                                <th>Date</th>
+                                <th>Status</th>
+                                <th>Reason</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>183</td>
+                                <td>John Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-success">Approved</span></td>
+                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                            </tr>
+                            <tr>
+                                <td>219</td>
+                                <td>Alexander Pierce</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-warning">Pending</span></td>
+                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                            </tr>
+                            <tr>
+                                <td>657</td>
+                                <td>Bob Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-primary">Approved</span></td>
+                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                            </tr>
+                            <tr>
+                                <td>175</td>
+                                <td>Mike Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-danger">Denied</span></td>
+                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                            </tr>
+                            <tr>
+                                <td>134</td>
+                                <td>Jim Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-success">Approved</span></td>
+                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                            </tr>
+                            <tr>
+                                <td>494</td>
+                                <td>Victoria Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-warning">Pending</span></td>
+                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                            </tr>
+                            <tr>
+                                <td>832</td>
+                                <td>Michael Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-primary">Approved</span></td>
+                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                            </tr>
+                            <tr>
+                                <td>982</td>
+                                <td>Rocky Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-danger">Denied</span></td>
+                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
             </div>
+            <!-- /.card -->
         </div>
     </div>
 
-</div>
 
-
-<?= $this->endSection(); ?>
+    <?= $this->endSection(); ?>
